05e11bdd9eeafed16a62c796a87e6c0a81d75704
cjs <1230cjs@gmail.com>
2024-05-25 15:01:09 +0900

    Initial commit

--- src/main/java/com/cjs/web/HelloController.java
+++ src/main/java/com/cjs/web/HelloController.java
@@ -1,5 +1,7 @@
 package com.cjs.web;
+import com.cjs.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +10,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         System.out.println("HelloController hello start");
+
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
