9b705bd544486669c9ad74330123f356de5e62e2
cjs <1230cjs@gmail.com>
2024-07-17 00:54:54 +0900

    Initial commit

--- src/main/java/com/cjs/Application.java
+++ src/main/java/com/cjs/Application.java
@@ -9,6 +9,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Application start");
         SpringApplication.run( Application.class, args);
-        //test3
+        //test4
     }
 }
